7fe83cb1e99a828da00e019bc75b826bc33c0722
João Barreto <108282056+JoaoBarreto03@users.noreply.github.com>
2024-07-11 22:40:34 -0300

    Questão 1489 Leetcode - Kruskal

---
+++ Questão 3/Questão 3.py
@@ -0,0 +1,61 @@
+class UnionFind:
+    def __init__(self, N):
+        self.parents = [x for x in range(N)]
+
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, a, b):
+        ua = self.find(a)
+        ub = self.find(b)
+
+        self.parents[ua] = ub
+
+class Solution:
+    def findCriticalAndPseudoCriticalEdges(self, N: int, edges: List[List[int]]) -> List[List[int]]:
+        E = len(edges)
+        e = sorted(enumerate(edges), key=lambda x: x[1][2])
+
+        # Krushal's algorithm
+        def mst(current_index, start):
+            uf = UnionFind(N)
+            count = 0
+            total = 0
+
+            if start:
+                su, sv, sw = edges[current_index]
+                uf.union(su, sv)
+                total += sw
+                count += 1
+            
+            for index, (u, v, w) in e:
+                if index == current_index:
+                    continue
+            
+                if uf.find(u) != uf.find(v):
+                    uf.union(u,v)
+                    total += w
+                    count += 1
+
+                if count == N-1:
+                    break
+
+            return total
+
+        base_mst = mst(-1, False)
+        critical = []
+        pseudocritical = []
+        for i in range(E):
+            if mst(i, True) == base_mst:
+                #this edge is part of at least one MST
+
+                if mst(i, False) != base_mst:
+                    critical.append(i)
+                else:
+                    pseudocritical.append(i)
+
+        return(critical, pseudocritical)
+
+
