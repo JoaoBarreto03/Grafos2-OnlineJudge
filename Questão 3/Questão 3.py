215d549d3bfab645dbff98e6f5d7a35ccf6efe84
João Barreto <108282056+JoaoBarreto03@users.noreply.github.com>
2024-07-14 21:56:25 -0300

    Simplificando codigo para evitar cálculos desnecessários no union e no mst

--- Questão 3/Questão 3.py
+++ Questão 3/Questão 3.py
@@ -1,61 +1,59 @@
 class UnionFind:
     def __init__(self, N):
-        self.parents = [x for x in range(N)]
+        self.parents = list(range(N))
 
     def find(self, x):
         if self.parents[x] != x:
-            self.parents[x] = self.find(self.parents[x])
+            self.parents[x] = self.find(self.parents[x])  # Path compression
         return self.parents[x]
     
     def union(self, a, b):
-        ua = self.find(a)
-        ub = self.find(b)
-
-        self.parents[ua] = ub
+        rootA = self.find(a)
+        rootB = self.find(b)
+        if rootA != rootB:
+            self.parents[rootB] = rootA
 
 class Solution:
     def findCriticalAndPseudoCriticalEdges(self, N: int, edges: List[List[int]]) -> List[List[int]]:
         E = len(edges)
         e = sorted(enumerate(edges), key=lambda x: x[1][2])
 
-        # Krushal's algorithm
-        def mst(current_index, start):
+        def mst(exclude_index=None, include_index=None):
             uf = UnionFind(N)
-            count = 0
             total = 0
+            edges_used = 0
 
-            if start:
-                su, sv, sw = edges[current_index]
-                uf.union(su, sv)
-                total += sw
-                count += 1
+            if include_index is not None:
+                u, v, w = edges[include_index]
+                uf.union(u, v)
+                total += w
+                edges_used += 1
             
             for index, (u, v, w) in e:
-                if index == current_index:
+                if index == exclude_index:
                     continue
-            
+                
                 if uf.find(u) != uf.find(v):
-                    uf.union(u,v)
+                    uf.union(u, v)
                     total += w
-                    count += 1
+                    edges_used += 1
 
-                if count == N-1:
-                    break
+                    if edges_used == N - 1:
+                        break
 
-            return total
+            if edges_used == N - 1:
+                return total
+            else:
+                return float('inf')
 
-        base_mst = mst(-1, False)
+        base_mst = mst()
         critical = []
         pseudocritical = []
+
         for i in range(E):
-            if mst(i, True) == base_mst:
-                #this edge is part of at least one MST
+            if mst(exclude_index=i) > base_mst:
+                critical.append(i)
+            elif mst(include_index=i) == base_mst:
+                pseudocritical.append(i)
 
-                if mst(i, False) != base_mst:
-                    critical.append(i)
-                else:
-                    pseudocritical.append(i)
-
-        return(critical, pseudocritical)
-
-
+        return [critical, pseudocritical]
